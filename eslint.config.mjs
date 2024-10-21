1bd442d52966399d12b98206fcc3889e09a383e9
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2024-10-20 22:42:11 -0300

    config: add Prettier in eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   // eslint react config
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: { ...globals.browser, ...globals.node },
+    },
     ...pluginReact.configs.flat.recommended,
     settings: {
       react: {
-        version: "detect",
-      }
+        version: 'detect',
+      },
     },
     plugins: {
       react: pluginReact.configs.flat.recommended.plugins.react,
@@ -23,13 +35,15 @@ export default [
     rules: {
       ...pluginReact.configs.flat.recommended.rules,
       ...eslintPluginReactHooks.configs.recommended.rules,
-      "no-console": "warn",
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-    }
+      'no-console': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': 'warn',
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettier,
 ];
